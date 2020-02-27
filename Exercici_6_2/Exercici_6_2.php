774455c969e7a99b98d254bd581bb913efa59f96
Borja Quílez <13841-8@AULESCIDET.LOCAL>
2020-02-27 17:30:00 +0100

    master

--- Exercici_6_2/Exercici_6_2.php
+++ Exercici_6_2/Exercici_6_2.php
@@ -18,24 +18,31 @@ function discount2($a,$b=0.15){
 
 $total=0;
     $item=$_GET["item"];
-    for($i=0;$i<count($item);$i++){
+    $item_name=$_GET["item_name"];
+    $item_quantity=$_GET["item_quantity"];
+    
+    for($i=0;$i<6;$i++){
         if (isset($item[$i])){
-            $total=$total+$item[$i];
-            echo "You have choosen a ".$item_nom[$i] ."<br>";
+            if (isset($item_quantity[$i])){
+                $total=$total+($item[$i]*$item_quantity[$i]);
+            }else{
+                $total=$total+$item[$i];
+            }
+            echo "You have choosen one ".$item_name[$i] ."<br>";
         }
     }
 
     if ($total<=1000){
-        echo "You are an scrooge an miserable person... Spent more money, you idiot!>
+        echo "You are an scrooge an miserable person... Spent more money, you idiot!<br>
         I won't apply any discount to this mean price... YOU OWE ME $total!";
     }elseif ($total>1000 and $total<=5000){
-        echo "Voy a hacerle un descuento del 10% por haber gastado hasta $total monedas!<br>
-        El precio le queda en:"; 
-        echo discount1($total); 
+        echo "I'm applying a 10% discount, because you're a good client. You've spent $total Gold pieces!<br>
+        Your amazing total amount, discount aplied is:"; 
+        echo discount1($total) ."Gold Pieces"; 
     }else{
-        echo "Le llamaremos el Derrochador! Le aplicaré un 15% por haber gastado hasta $total monedas!<br>
-        El precio le queda en:";
-        echo discount2($total); 
+        echo "I'm calling you 'The Spendaholic'!!! You're my greatest client, so you deserve the gratest discount ever!!!<br>
+        You've spent $total, but I'm giving you a 15% discount, so you only have to pay:";
+        echo discount2($total) ."Gold Pieces."; 
     }
 
 ?> 
